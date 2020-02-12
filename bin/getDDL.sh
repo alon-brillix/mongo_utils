f199eaacadd8bf7fb01928d607a7f7ee11443af4
alon <alon@Roni-Tomcat-NAM1.(none)>
2020-02-12 14:21:30 +0000

    added getDDL.sh to extract DDL of a collection

---
+++ bin/getDDL.sh
@@ -0,0 +1,89 @@
+usage() {
+	local str
+	read -d '' str <<-EOF
+		Usage $(basename $0) [OPTION]...
+		Use this tool to monitor the count of a collection.
+		The tool will draw an ongoing graph with icrease(red) and decrease(green) of the count
+
+		OPTIONS
+		-h|--help			help - print this help page
+		-c|--collection	collection - which collection to generate the DDL for
+		-d|--database		database - where the collection resides (default to [${MONGO_DB}])
+		-p|--pretty			Add prettyfing comments
+	EOF
+
+	echo -e "${str}\n"
+	exit
+}
+
+RED() {
+	local text=$*
+	echo -e "\e[31m${text}\e[0m"
+}
+GREEN() {
+	local text=$*
+	echo -e "\e[32m${text}\e[0m"
+}
+
+
+. ~/.mongo.env
+
+getDir() {
+	local SOURCE="${BASH_SOURCE[0]}"
+	local DIR
+	while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+	  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+	  SOURCE="$(readlink "$SOURCE")"
+	  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+	done
+	DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+	echo ${DIR}
+}
+
+getData() {
+	mongo --quiet --host ${MONGOS} -u ${MONGO_USER} -p ${MONGO_PASSWORD} --authenticationDatabase admin ${MONGO_DB} --eval "load('${DIR}/../scripts/getDDL.js'); getDDL('${collection}', ${pretty})"
+}
+
+pretty=false
+while getopts ":ht:w:c:d:-:" opt; do
+	case ${opt} in
+		c)
+			collection=${OPTARG}
+			;;
+		d)
+			MONGO_DB=${OPTARG}
+			;;
+		p)
+			pretty=true
+			;;
+		-)
+			OPT=$(echo ${OPTARG}|cut -d'=' -f1)
+			VAL=$(echo ${OPTARG}|cut -d'=' -f2)
+			case ${OPT} in
+				pretty)
+					pretty=true
+					;;
+				collection)
+					collection=${VAL}
+					;;
+				database)
+					MONGO_DB=${VAL}
+					;;
+			esac
+			;;
+		h)
+			usage
+			;;
+		\?)
+			echo "Invalid option: -${OPTARG}" >&2
+			usage
+			;;
+		esac
+done
+
+if [[ ${collection} == "" ]];then
+	echo -e "$(RED Mandatory argument [collection] is missing!)"
+	usage
+fi
+DIR=$(getDir)
+getData
---
+++ scripts/getDDL.js
@@ -0,0 +1,38 @@
+function getDDL(collection, pretty) {
+	var database = db.getName();
+	var indexes = db[collection].getIndexes();
+	var shardSpec = db.getSiblingDB('config').collections.findOne({_id: `${database}.${collection}`});
+	var DDL = {};
+	var indexOptions;
+	DDL.createCollection = `db.createCollection("${collection}")`;
+	if (shardSpec !== null) {
+		DDL.shard = {
+			shardCollection: `${database}.${collection}`,
+			key: shardSpec.key,
+			unique: shardSpec.unique,
+			numInitialChunks: shardSpec.numInitialChunks
+		};
+		(DDL.shard.numInitialChunks === null) && delete DDL.shard.numInitialChunks
+	}
+	DDL.indexes = [];
+	for (var i = 0; i < indexes.length; i++){
+		if (indexes[i].name !== "_id_") {
+			indexOptions = {};
+			for (var key of Object.keys(indexes[i])){
+				if (["v", "key", "ns"].indexOf(key) === -1){
+					indexOptions[key] = indexes[i][key];
+				}
+			}
+			DDL.indexes.push(`db.${collection}.createIndex(${JSON.stringify(indexes[i].key)}, ${JSON.stringify(indexOptions)})`);
+		}
+	}
+	var output = "";
+	if (pretty === true) output += `/*** ${database}.${collection} ***/\n`
+	output += `${DDL.createCollection};\n`
+	if (DDL.shard !== undefined) output += `db.adminCommand(${JSON.stringify(DDL.shard)});\n`
+	for (var i = 0; i < DDL.indexes.length; i++){
+		output += `${DDL.indexes[i]};`
+	}
+	if (pretty === true) output += `\n/*-- ${database}.${collection} --*/`
+	return output;
+}
